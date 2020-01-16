21c0f16e4d20790d1883e6f83737052103864769
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-01-16 16:35:24 +0100

    fix: Add full screen mode for FirstConnexion activity and Connexion Activity

--- app/src/main/java/com/example/mycomifclient/connexion/FirstConnexionActivity.kt
+++ app/src/main/java/com/example/mycomifclient/connexion/FirstConnexionActivity.kt
@@ -2,6 +2,7 @@ package com.example.mycomifclient.connexion
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
@@ -36,5 +37,25 @@ class FirstConnexionActivity : AppCompatActivity() {
             builder.create()
         }
         alertDialog?.show()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
