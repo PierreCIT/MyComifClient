235a10d0c1c544e790056c31d3eddb9e4d20ea08
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-01-26 15:41:19 +0100

    add: Adding doc for ViewPagerAdapter

--- app/src/main/java/com/example/mycomifclient/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/mycomifclient/ViewPagerAdapter.kt
@@ -4,6 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
+/**
+ * Implementation of a ViewPagerAdapter for transaction list
+ * @param supportFragmentManager fragment manager
+ */
 class ViewPagerAdapter(supportFragmentManager: FragmentManager) :
     FragmentStatePagerAdapter(supportFragmentManager) {
 
@@ -22,6 +26,12 @@ class ViewPagerAdapter(supportFragmentManager: FragmentManager) :
         return mFragmentTitleList[position]
     }
 
+    /**
+     * Add a fragment to the fragment manager
+     * @param fragment Fragment you want to add into the manager
+     * @param title Title of the fragment you want to add
+     * @return None
+     */
     fun addFragment(fragment: Fragment, title: String) {
         mFragmentList.add(fragment)
         mFragmentTitleList.add(title)
