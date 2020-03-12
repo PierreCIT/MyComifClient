ae0c6d81d71e41446817c720606a30b98e900eb7
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-03-12 19:20:56 +0100

    fix: fixing numerous minor display bugs

--- app/src/main/java/com/example/mycomifclient/MainActivity.kt
+++ app/src/main/java/com/example/mycomifclient/MainActivity.kt
@@ -160,11 +160,6 @@ class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionList
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
     /**
      * Set the shared preference connexion status var
      * @param bool Connexion status (True = connected; false = not connected) (Boolean)
@@ -174,25 +169,6 @@ class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionList
         val editor = sharedPref.edit()
         editor.putBoolean(CONNEXION_STATUS_KEY, bool)
         editor.apply()
-    }
-
-    /**
-     * Enable immersive mode
-     * @return None
-     */
-    private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 
     private fun logout() {
